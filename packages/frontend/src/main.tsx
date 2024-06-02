4b6d2767b124b25fcb950b281f115b26207d3c50
ZakThink <zak@thinkable.co>
2024-06-02 12:45:54 +0100

    feat: added aws amplify to the frontend

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -4,6 +4,29 @@ import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Amplify } from "aws-amplify";
+import config from "./config.ts";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolClientId: config.cognito.APP_CLIENT_ID,
+    },
+  },
+  Storage: {
+    S3: { region: config.s3.REGION, bucket: config.s3.BUCKET },
+  },
+  API: {
+    REST: {
+      notes: {
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
